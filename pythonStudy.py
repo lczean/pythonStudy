ec27e185e224a24da65e90c7b81cdc1a866d1d92
lczean <lczean@gmail.com>
2017-04-21 14:33:40 +0800

    add pythonStudy.py

---
+++ pythonStudy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-21 14:29:22
+# @Author  : lczean (lczean@163.com)
+# @Link    : https://github.com/lczean
+# @Version : 1.0
+
+
+print("Learning PythonStudy!")
