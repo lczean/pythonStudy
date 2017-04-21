f77baac6f476339d18dac70b6e05170847e3e950
lczean <lczean@163.com>
2017-04-21 17:17:14 +0800

    add testPython2.py

---
+++ testPython2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-04-21 17:15:19
+# @Author  : lczean (lczean@163.com)
+# @Link    : https://github.com/lczean
+# @Version : 1.0
+
+import os
+
+print(os.getcwd)
+print("hello lczean!")
