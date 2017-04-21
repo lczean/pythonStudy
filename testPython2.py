a439b8988f704bb4a2049566509281d65a15f793
lczean <lczean@163.com>
2017-04-21 17:29:23 +0800

    add git push operation

--- testPython2.py
+++ testPython2.py
@@ -4,8 +4,15 @@
 # @Author  : lczean (lczean@163.com)
 # @Link    : https://github.com/lczean
 # @Version : 1.0
+"""
+No.1: ctrl+shift+p -> gaa <git Add All> 
+No.2: ctrl+shift+p -> gs <git status> -> press "c" -> input a label(标注) -> ctrl+W 关闭当前的日志文件后会自动触发commit提交操作 -> 再次 ctrl+W 关闭status日志文件
+No.3: ctrl+shift+p -> gp <git Push> -> 推送代码到宿主master中
+"""
 
 import os
 
 print(os.getcwd)
 print("hello lczean!")
+
+
