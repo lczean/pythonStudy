06123a3d1b6aeb6925dcf499b130662900dc1cc4
lczean <lczean@163.com>
2017-05-02 15:22:31 +0800

    create an instance folder and create a test file

---
+++ example/helloworld.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-05-02 15:14:29
+# @Author  : lczean (lczean@163.com)
+# @Link    : https://github.com/lczean
+# @Version : 1.0
+
+
+print("Hello World!")
