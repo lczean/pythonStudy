b13eabc47ef8fbf426b20453ee0e2cb34e679869
lczean <lczean@163.com>
2017-05-03 10:48:07 +0800

    add square_root.py

---
+++ example/square_root.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-05-03 09:57:18
+# @Author  : lczean (lczean@163.com)
+# @Link    : https://github.com/lczean
+# @Version : 1.0
+# @File    : square_root.py
+
+
+num = float(input('请输入一个数字：'))
+num_sprt = num ** 0.5
+
+print(' %0.3f 的平方根为 %0.3f ' % (num, num_sqrt))
