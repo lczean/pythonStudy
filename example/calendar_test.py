99ccf3227459c984b2dd70e9d98366d1d4dad405
lczean <lczean@163.com>
2017-05-04 10:07:38 +0800

    modify calendar_test.py

--- example/calendar_test.py
+++ example/calendar_test.py
@@ -6,13 +6,19 @@
 # @Version : 1.0
 # @File    : calendar_test.py
 
+'''
+# add docstring 说明
+# set the variables：
+    1.year：YY
+    2.mouth: MM
+'''
 
 # 引人日历模块
 import calendar
 
-# 输入指定年月
-yy = int(input('输入年份：'))
-mm = int(input('输入月份：'))
+# 输入指定年月，常量必须大写，否则违法命名规范
+YY = int(input('输入年份：'))
+MM = int(input('输入月份：'))
 
 # 显示日历
-print(calendar.month(yy, mm))
+print(calendar.month(YY, MM))
