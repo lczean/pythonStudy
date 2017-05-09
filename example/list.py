a6a688133b5ccb9ba4abc2c4faa9ab97de3611fd
lczean <lczean@163.com>
2017-05-09 17:12:32 +0800

    modify list.py

--- example/list.py
+++ example/list.py
@@ -25,7 +25,9 @@ MOVIES = ["肖申克的救赎", 1994, "弗兰克·德拉邦特", 142,
 # No.1
 for each_item in MOVIES:
     print(each_item)
+
 print("***************")
+
 # No.2
 for each_item in MOVIES:
     if isinstance(each_item, list):
@@ -33,15 +35,38 @@ for each_item in MOVIES:
             print(nested_item)
     else:
         print(each_item)
-# No.3
+
 print("***************")
+
+# No.3
 for each_item in MOVIES:
     if isinstance(each_item, list):
         for nested_item in each_item:
             if isinstance(nested_item, list):
-                for nested_item_2 in nested_item:
-                    print(nested_item_2)
+                for deeper_item in nested_item:
+                    if isinstance(deeper_item, list):
+                        for deepest_item in deeper_item:
+                            print(deepest_item)
+                    else:
+                        print(deeper_item)
             else:
                 print(nested_item)
     else:
         print(each_item)
+
+print("***************")
+
+# No.4
+# 不重复代码，应当创建一个函数
+# def 函数名 (参数，可选)
+#   函数代码组
+
+
+def print_lists(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lists(each_item) #递归,python默认递归深度不能超过100个
+        else:
+            print(each_item)
+
+print_lists(MOVIES)
